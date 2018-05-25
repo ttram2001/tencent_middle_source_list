c0dec3b0b48ad7437b8f9e97ea4786fe1fccb158
root <tritram@yahoo.com>
2018-05-26 05:18:54 +0800

    Added create_middle_source_list.py

---
+++ create_middle_source_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import datetime
+import json
+import subprocess
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+
+db = client.middle_source_list
+collection = db['ip']
+
+cursor = db.ip.find().sort('ip')
+counter = 1
+currentDT = datetime.datetime.now()
+print '<html> <title> Tencent Middle Source List </title>'
+print '<body>'
+print '<h1> Tencent Middle Source List as of ' + str(currentDT) + '</h1>'
+print '<table border=1>'
+print '<th> Number </th><th> IP Address </th><th> Datetime </th>'
+for document in cursor:
+	print '<tr>'
+	print '<td>' + str(counter) + '</td><td>' + document['ip'] + '</td><td> ' + str(currentDT) + '</td>'
+	print '</tr>'
+	counter = counter + 1
+
+print '</table>'
+print '</body>'
+print '</html>'
